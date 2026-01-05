de22ff48ab6c623f445233171bc144fbe5e19314
Revanth-769 <revanthvinay17@outlook.com>
2026-01-04 20:14:17 -0400

    chatbot first commit

---
+++ app/pdf_utils.py
@@ -0,0 +1,12 @@
+# once files are uploaded this module will help in reading the information the files.
+
+from pypdf import  PdfReader
+from typing import List, Optional
+from io import BytesIO
+
+def extract_text_from_pdf(file):
+    reader = PdfReader(file)
+    text = ' '
+    for page in reader.pages:
+        text = text + page.extract_text() or ''
+    return text
